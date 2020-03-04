9db7771522bc21356d52f0a8761c89e44941884d
nehabalaji <nehabalaji10@gmail.com>
2020-03-04 17:38:16 +0530

    final changes

--- Lesson03-Green-Recycler-View/T03.05-Exercise-GoingGreen/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Lesson03-Green-Recycler-View/T03.05-Exercise-GoingGreen/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -40,7 +40,7 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
     private static final String TAG = GreenAdapter.class.getSimpleName();
 
     // TODO (8) Add a private static int called viewHolderCount that will hold the total number of ViewHolders that are created
-
+    private static int viewHolderCount;
     private int mNumberItems;
 
     /**
