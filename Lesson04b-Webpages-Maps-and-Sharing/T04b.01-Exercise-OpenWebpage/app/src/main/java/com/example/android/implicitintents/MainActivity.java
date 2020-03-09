e075bded3583f96cf228e7bd8a1db1ed3f076975
nehabalaji <nehabalaji10@gmail.com>
2020-03-09 17:10:04 +0530

    add website intent

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -15,10 +15,14 @@
  */
 package com.example.android.implicitintents;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,9 +40,9 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickOpenWebpageButton(View v) {
         // TODO (5) Create a String that contains a URL ( make sure it starts with http:// or https:// )
-
+        String url = "https://developer.android.com/guide/components/intents-common.html";
         // TODO (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
-        Toast.makeText(this, "TODO: Open a web page when this button is clicked", Toast.LENGTH_SHORT).show();
+        openWebPage(url);
     }
 
     /**
@@ -79,10 +83,13 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a method called openWebPage that accepts a String as a parameter
     // Do steps 2 - 4 within openWebPage
-
+    public void openWebPage(String url){
+        Uri webPage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webPage);
+        if(intent.resolveActivity(getPackageManager())!=null)
+            startActivity(intent);
+    }
         // TODO (2) Use Uri.parse to parse the String into a Uri
-
         // TODO (3) Create an Intent with Intent.ACTION_VIEW and the webpage Uri as parameters
-
         // TODO (4) Verify that this Intent can be launched and then call startActivity
 }
