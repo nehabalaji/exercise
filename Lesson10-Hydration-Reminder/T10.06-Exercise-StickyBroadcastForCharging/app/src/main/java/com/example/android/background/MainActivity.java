40b53c36ad4c32083050c1299fa5eb0f5976e011
nehabalaji <nehabalaji10@gmail.com>
2020-05-13 13:37:28 +0530

    add method to get current charging status

--- Lesson10-Hydration-Reminder/T10.06-Exercise-StickyBroadcastForCharging/app/src/main/java/com/example/android/background/MainActivity.java
+++ Lesson10-Hydration-Reminder/T10.06-Exercise-StickyBroadcastForCharging/app/src/main/java/com/example/android/background/MainActivity.java
@@ -20,6 +20,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -87,7 +89,19 @@ public class MainActivity extends AppCompatActivity implements
             // TODO (2) Get a BatteryManager instance using getSystemService()
             // TODO (3) Call isCharging on the battery manager and pass the result on to your show
             // charging method
+        if(Build.VERSION.SDK_INT==Build.VERSION_CODES.M)
+        {
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            showCharging(batteryManager.isCharging());
+        }
+        else{
+            IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            Intent intent = registerReceiver(null, intentFilter);
 
+            int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            Boolean b = (status==BatteryManager.BATTERY_STATUS_CHARGING || status==BatteryManager.BATTERY_STATUS_FULL);
+                showCharging(b);
+        }
         // TODO (4) If your user is not on M+, then...
             // TODO (5) Create a new intent filter with the action ACTION_BATTERY_CHANGED. This is a
             // sticky broadcast that contains a lot of information about the battery state.
